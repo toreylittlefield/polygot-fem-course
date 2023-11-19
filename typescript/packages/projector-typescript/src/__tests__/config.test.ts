60d812205a91ca65e5e23edf18399aec54c5200f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2023-11-19 22:03:17 +0800

    print, add, remove unit tests

--- typescript/packages/projector-typescript/src/__tests__/config.test.ts
+++ typescript/packages/projector-typescript/src/__tests__/config.test.ts
@@ -5,3 +5,21 @@ test('simple print all', () => {
   expect(configuration.operation).toEqual(OPERATIONS.PRINT);
   expect(configuration.args).toEqual([]);
 });
+
+test('print key', () => {
+  const configuration = config({ args: ['foo'] });
+  expect(configuration.operation).toEqual(OPERATIONS.PRINT);
+  expect(configuration.args).toEqual(['foo']);
+});
+
+test('add key', () => {
+  const configuration = config({ args: ['add', 'foo', 'bar'] });
+  expect(configuration.operation).toEqual(OPERATIONS.ADD);
+  expect(configuration.args).toEqual(['foo', 'bar']);
+});
+
+test('remove key', () => {
+  const configuration = config({ args: ['remove', 'foo'] });
+  expect(configuration.operation).toEqual(OPERATIONS.REMOVE);
+  expect(configuration.args).toEqual(['foo']);
+});
